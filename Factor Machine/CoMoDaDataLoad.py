958216d7b5ac3abd2787647feed644860749db8a
xfeng82 <xfeng82@wisc.edu>
2020-11-29 08:42:05 -0600

    内存不够，重新考虑特征

--- Factor Machine/CoMoDaDataLoad.py
+++ Factor Machine/CoMoDaDataLoad.py
@@ -10,10 +10,11 @@ def featureMatrix(path,userFeature,itemFeature):
     file=file.replace(-1,0)
     user_item=np.array(pd.DataFrame(file,columns=['userID','itemID']))
     data=np.array(pd.DataFrame(file,columns=userFeature+itemFeature))
-    oneHot=[]
-    for sample in user_item:
-        oneHot.append(UserItemOneHot(sample[0],sample[1],path))
-    data=np.append(np.array(oneHot),data,axis=1)
+    # oneHot=[]
+    # for sample in user_item:
+    #     oneHot.append(UserItemOneHot(sample[0],sample[1],path))
+    # data=np.append(np.array(oneHot),data,axis=1)
+    data=np.append(user_item,data,axis=1)
     data=np.append(data,np.ones([len(data),1]),axis=1)
     return data
 
@@ -46,14 +47,23 @@ def negativeSample(path,userFeature,itemFeature,ratio):
     while negaNum<ratio*len(item_pool):
         usr,item=np.random.randint(len(user_pool)),np.random.randint(len(item_pool))
         if item_pool[item] not in userItem[user_pool[usr]]:
-            oneHot=UserItemOneHot(usr,item,path)
-            temp=oneHot+list(userRela[usr])+list(itemRela[item])+[-1]
+            # oneHot=UserItemOneHot(usr,item,path)
+            # temp=oneHot+list(userRela[usr])+list(itemRela[item])+[-1]
+            temp =list(userRela[usr]) + list(itemRela[item]) + [-1]
             negaSample.append(temp)
             negaNum+=1
     return np.array(negaSample)
 
-
-
+# path='/Users/user/git_repo/Movie-Recommender-System/CoMoDa/CoMoDa.csv'
+#
+# userFeature=['age','sex','city','country','daytype','season','location','weather','social','endEmo','dominantEmo','mood','physical','decision','interaction']
+# itemFeature=['director','movieCountry','movieLanguage','movieYear','genre1','genre2','genre3','actor1','actor2','actor3','budget']
+# neg=negativeSample(path,userFeature,itemFeature,1)
+# pos=featureMatrix(path,userFeature,itemFeature)
+# with open('positiveData.pickle','wb') as f:
+#     pickle.dump(pos,f)
+# with open('negativeData.pickle','wb') as f:
+#     pickle.dump(neg,f)
 
 
 def prepareData():
